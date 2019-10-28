cdbc292731440334a1053bc28839a41139eb98e0
keith <mudstuffing@gmail.com>
2019-10-27 20:23:54 -0400

    wip

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexORM from '@vuex-orm/core'
+import User from '../models/User'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const database = new VuexORM.Database()
+
+database.register(User)
+
+const store = new Vuex.Store({
+  plugins: [VuexORM.install(database)]
 })
+
+export default store
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ users }}
   </div>
 </template>
+
+<script>
+
+import User from '../models/User'
+
+export default{
+	props:[],
+	data: function(){
+		return {
+
+		}
+	},
+	computed : {
+		users(){
+			return User.all();
+		}
+	},
+	mounted : function(){
+		// Assuming this data structure is the response from the API backend.
+      const posts = [
+        {
+          id: 1,
+          name : 'Keith',
+          email : 'keith@paleosun.com'
+        }
+      ]
+
+      User.insert({ data: posts })
+	},
+}
+
+</script>
